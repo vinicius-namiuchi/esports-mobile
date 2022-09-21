d615ff866cbea7044be982b644b0e48dee6c7f37
Vinicius Silva <vinicius.namiuchi@gmail.com>
2022-09-21 10:03:27 -0300

    Finalizado aplicativo

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Image, TouchableOpacity, View, FlatList } from 'react-native';
+import { Image, TouchableOpacity, View, FlatList, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import { Entypo } from '@expo/vector-icons';
@@ -14,10 +14,12 @@ import { GameParams } from '../../@types/navigation';
 import { Heading } from '../../components/Heading';
 import { Background } from '../../components/Background';
 import { DuoCard, DuoCardProps } from '../../components/DuoCard';
+import { DuoMatch } from '../../components/DuoMatch'
 
 export function Game() {
 
     const [duos, setDuos] = useState<DuoCardProps[]>([]);
+    const [discordDuoSelected, setDiscordDuoSelected] = useState('');
 
     const navigation = useNavigation();
     const route = useRoute();
@@ -25,6 +27,12 @@ export function Game() {
 
     function handleGoBack () {
         navigation.goBack();
+    }
+
+    async function getDiscordUser(adsId: string) {
+        fetch(`http://192.168.6.164:3333/ads/${adsId}/discord`)
+        .then(response => response.json())
+        .then(data => setDiscordDuoSelected(data.discord));
     }
 
     useEffect(() => {
@@ -69,15 +77,25 @@ export function Game() {
                     renderItem={({ item }) => (
                         <DuoCard
                             data={item}
-                            onConnect={() => {}}
+                            onConnect={() => getDiscordUser(item.id)}
                         />
                     )}
                     horizontal
                     style={styles.containerList}
-                    contentContainerStyle={styles.contentList}
+                    contentContainerStyle={[duos.length > 0 ? styles.contentList : styles.emptyListContent]}
                     showsHorizontalScrollIndicator={false}
+                    ListEmptyComponent={() => (
+                        <Text style={styles.emptyListText}>
+                            Não há anúncios publicados ainda.
+                        </Text>
+                    )}
                 />
 
+                <DuoMatch
+                    visible={discordDuoSelected.length > 0}
+                    discord={discordDuoSelected}
+                    onClose={() => setDiscordDuoSelected('')}
+                />
             </SafeAreaView>
         </Background>
     );
